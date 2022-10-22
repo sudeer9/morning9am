06e249f2f93a1eea9981d7a1193b9c56d27d9251
sudeerkumara.s <sudeerkumara.s@BRILAP2989.BRILLIO.COM>
2022-10-22 09:04:57 +0530

    Adding and modifiying

--- src/p1/Demo1.java
+++ src/p1/Demo1.java
@@ -1,11 +1,9 @@
 package p1;
 
-public class Demo {
+public class Demo1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		System.out.println("gdajd");
 
 	}
 
